12e2a0217d8139194f556385c62b7c5986808d62
A-Hirwa <a.hirwa1@alustudent.com>
2024-09-23 18:16:13 +0200

    Create directory structure and initial file creation

---
+++ create_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir submission_reminder_app
+cd submission_reminder_app
+mkdir app modules assets config 
+touch app/reminder.sh
+touch modules/functions.sh
+touch assets/submissions.txt
+touch config/config.env
+touch startup.sh
